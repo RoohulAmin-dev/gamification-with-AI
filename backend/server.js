e937600eeef4190cabeab0704d9b496748671d48
RoohulAmin-dev <rohulameen1544@gmail.com>
2026-08-09 10:57:49 -0700

    Sever code is changed, to make it compatible with vercel serverless environment.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const { app, PORT } = require("./src/app");
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
