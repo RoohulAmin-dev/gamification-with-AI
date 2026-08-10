681b28ecf24c54fe41db9db16b6d4c7efaa7c345
RoohulAmin-dev <rohulameen1544@gmail.com>
2026-08-10 05:29:56 -0700

    Changed the frontend end point to easily communicate with backend server.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://gamification-with-4gdj9vbap-roohul-amin.vercel.app/api',
   timeout: 60000,
   headers: {
     'Content-Type': 'application/json',
